a8f117fff49255ff2e9707621de6f4a0631d8fb4
liaoyinglong <879015147@qq.com>
2017-06-14 23:49:51 +0800

    移动端页脚

--- src/jsx/components/mobile/mobile_index.jsx
+++ src/jsx/components/mobile/mobile_index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+
 import MobileHeader from './mobile_header'
+import MobileFooter from './mobile_Footer'
+
+
 export default class MobileIndex extends React.Component {
   render() {
     return (
       <div id='mobile'>
         <MobileHeader />
+        <MobileFooter />
       </div>
     )
   }
