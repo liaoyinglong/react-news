ff70e9aa7a3f0bc359cfad9f691ebef52492c945
liaoyinglong <879015147@qq.com>
2017-06-15 01:14:58 +0800

    login dialog finshed

--- src/jsx/components/pc/pc_header.jsx
+++ src/jsx/components/pc/pc_header.jsx
@@ -1,20 +1,57 @@
 import React from 'react'
 
-import { Row, Col, Menu, Icon } from 'antd'
-
+import { Row, Col, Menu, Icon, Tabs, message, Form, Input, Button, Checkbox, Modal } from 'antd'
+const TabPane = Tabs.TabPane
+const FormItem = Form.Item
 const SubMenu = Menu.SubMenu
 const MenuItemGroup = Menu.ItemGroup
 
 import 'stylus/pc/pc_header'
 
-export default class PCHeader extends React.Component {
+class PCHeader extends React.Component {
   constructor() {
     super()
     this.state = {
-      current: 'top'
+      current: 'top',
+      modalVisible: false,
+      action: 'login',
+      hasLogined: false,
+      userNickName: '',
+      userId: 0
     }
   }
+  // this.setState is a function
+  setModalVisible(value) {
+    this.setState({ modalVisible: value })
+  }
+  handleSubmit() {
+  
+  }
+  handleClick(e) {
+    if (e.key === 'register') {
+      this.setState({ current: 'register' })
+      this.setModalVisible(true)
+      return
+    }
+    this.setState({ current: e.key })
+  }
   render() {
+    let { getFieldDecorator } = this.props.form
+    const userShow = this.state.hasLogined
+      ?
+      <Menu.Item key='logout' className='register'>
+        <Button type='primary' htmlType='button'>{this.state.userNickName}</Button>
+        &nbsp;&nbsp;
+        <Link target='_blank'>
+          <Button type='dashed' htmlType='button'>个人中心</Button>
+        </Link>
+        &nbsp;&nbsp;
+        <Button type='ghost' htmlType='button'>退出</Button>
+      </Menu.Item>
+      :
+      <Menu.Item key='register' className='register'>
+        <Icon type='login' />注册 / 登录
+      </Menu.Item>
     return (
       <header>
         <Row>
@@ -26,7 +63,7 @@ export default class PCHeader extends React.Component {
             </a>
           </Col>
           <Col span={16}>
-            <Menu mode='horizontal' selectedKeys={[this.state.current]}>
+            <Menu mode='horizontal' selectedKeys={[this.state.current]} onClick={this.handleClick.bind(this)}>
               <Menu.Item key='top'><Icon type='appstore' /> 头条</Menu.Item>
               <Menu.Item key='shehui'><Icon type='appstore' /> 社会</Menu.Item>
               <Menu.Item key='guonei'><Icon type='appstore' /> 国内</Menu.Item>
@@ -35,7 +72,26 @@ export default class PCHeader extends React.Component {
               <Menu.Item key='tiyu'><Icon type='appstore' /> 体育</Menu.Item>
               <Menu.Item key='keji'><Icon type='appstore' /> 科技</Menu.Item>
               <Menu.Item key='shishang'><Icon type='appstore' /> 时尚</Menu.Item>
+              {userShow}
             </Menu>
+            <Modal title='用户中心' wrapClassName='vertical-center-modal' visible={this.state.modalVisible} onCancel={() => this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText='关闭'>
+              <Tabs type='card'>
+                <TabPane tab='注册' key='2'>
+                  <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                    <FormItem label='账户'>
+                      <Input placeholder='请输入您的账号' {...getFieldDecorator('r_userName') } />
+                    </FormItem>
+                    <FormItem label='密码'>
+                      <Input type='password' placeholder='请输入您的密码' {...getFieldDecorator('r_password') } />
+                    </FormItem>
+                    <FormItem label='确认密码'>
+                      <Input type='password' placeholder='请确认您的密码' {...getFieldDecorator('r_confirmPassword') } />
+                    </FormItem>
+                    <Button type='primary' htmlType='submit'>注册</Button>
+                  </Form>
+                </TabPane>
+              </Tabs>
+            </Modal>
           </Col>
           <Col span={2}></Col>
           <Col span={2}></Col>
@@ -44,3 +100,5 @@ export default class PCHeader extends React.Component {
     )
   }
 }
+// 要做二次封装 https://ant.design/components/form-cn/#Form.create(options)
+export default PCHeader = Form.create({})(PCHeader)
