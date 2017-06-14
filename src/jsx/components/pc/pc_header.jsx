45975f2bf60bca52d10202a67bb47cdef4b865cb
liaoyinglong <879015147@qq.com>
2017-06-14 20:54:17 +0800

    pc端导航menu over

--- src/jsx/components/pc/pc_header.jsx
+++ src/jsx/components/pc/pc_header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Row, Col } from 'antd'
 
+import { Row, Col, Menu, Icon } from 'antd'
+
+const SubMenu = Menu.SubMenu
+const MenuItemGroup = Menu.ItemGroup
 
 import 'stylus/pc_header'
 
 export default class PCHeader extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      current: 'top'
+    }
+  }
   render() {
     return (
       <header>
@@ -12,8 +21,21 @@ export default class PCHeader extends React.Component {
           <Col span={2}></Col>
           <Col span={4}>
             <a href="/" className='logo'>
-              <img src="/src/images/logo.png" alt="" />
+              <img src="/src/images/logo.png" alt="logo" />
+              <span>ReactNews</span>
             </a>
+          </Col>
+          <Col span={16}>
+            <Menu mode='horizontal' selectedKeys={[this.state.current]}>
+              <Menu.Item key='top'><Icon type='appstore' /> 头条</Menu.Item>
+              <Menu.Item key='shehui'><Icon type='appstore' /> 社会</Menu.Item>
+              <Menu.Item key='guonei'><Icon type='appstore' /> 国内</Menu.Item>
+              <Menu.Item key='guoji'><Icon type='appstore' /> 国际</Menu.Item>
+              <Menu.Item key='yule'><Icon type='appstore' /> 娱乐</Menu.Item>
+              <Menu.Item key='tiyu'><Icon type='appstore' /> 体育</Menu.Item>
+              <Menu.Item key='keji'><Icon type='appstore' /> 科技</Menu.Item>
+              <Menu.Item key='shishang'><Icon type='appstore' /> 时尚</Menu.Item>
+            </Menu>
           </Col>
           <Col span={2}></Col>
           <Col span={2}></Col>
